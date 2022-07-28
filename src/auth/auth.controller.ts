9008354c90f16e79f69c7c5a6e50cb25ed6bde4c
Mohd Noor Shawal <37688393+mohdnoorshawal@users.noreply.github.com>
2022-07-28 17:25:35 +0800

    Pipe and Argon

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,4 @@
-import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
-import { type } from 'os';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
@@ -9,7 +8,7 @@ export class AuthController {
 
   @Post('signup')
   signup(@Body() dto: AuthDto) {
-    return this.authService.signup();
+    return this.authService.signup(dto);
   }
 
   @Post('signin')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
-
   constructor(private prisma: PrismaService) {}
 
-  signup() {
+  signup(dto: AuthDto) {
+    //generate password hash
+
+    //save new user in the db
+
+    //return the saved user
+    
     return {
       msg: 'I have signed up',
     };
