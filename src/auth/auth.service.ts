7df509c4398da7d9f3b02d7697ba6669baeec6e3
Mohd Noor Shawal <37688393+mohdnoorshawal@users.noreply.github.com>
2022-08-15 11:22:57 +0800

    Sign In Logic & Config Module set-up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -39,9 +39,29 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return {
-      msg: 'I have signed in',
-    };
+  async signin(dto: AuthDto) {
+    //find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    //if user does not exist, throw exception
+    if (!user) {
+      throw new ForbiddenException('Credentials Incorrect!');
+    }
+
+    //compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    //if password incorrect, throw exception
+    if (!pwMatches) {
+      throw new ForbiddenException('Credentials Incorrect!');
+    }
+
+    //send back the user
+    delete user.hash;
+    return user;
   }
 }
